0758d5dd6452d19943d7fe749c91c3a1a9e673c8
Caleb Johnson <me@calebj.io>
2016-09-24 00:24:32 -0500

    [red] SSHD fixes

--- red-sshd/sshd.sh
+++ red-sshd/sshd.sh
@@ -4,6 +4,21 @@ if [ ! -d /var/run/sshd ]; then
     mkdir /var/run/sshd
     chmod 0755 /var/run/sshd
 fi
+
+if [ -e /data/ssh ]; then
+    mv /etc/ssh /etc/ssh_dist
+fi
+
+if [ ! -e /data/ssh ]; then
+    mkdir -p /data/ssh
+    chown 1000:1000 /data
+    ln -s /etc/ssh /data/ssh/config
+fi
+
+if [ ! -e /etc/ssh/sshd_config ]; then
+    cp -a /etc/ssh_dist/* /etc/ssh/
+fi
+
 if [ -e /etc/ssh/sshd_not_to_be_run ]; then
     echo "/etc/ssh/sshd_not_to_be_run present; not starting SSHD"
     exit 0
